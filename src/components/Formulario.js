ee4ef3691dfcf92f91af0d581d21ce778836f4c7
Martin Gonzalez <gonzalez.martin.mag@gmail.com>
2021-07-07 01:17:48 -0300

    Agrego Formulario component

---
+++ src/components/Formulario.js
@@ -0,0 +1,68 @@
+import React, { Fragment, useState } from 'react';
+
+const Formulario = () => {
+
+    // Crear state de citas
+    const [cita, actualizarCita] = useState({
+        mascota: '',
+        propietario: '',
+        fecha: '',
+        hora: '',
+        sintomas: ''
+    });
+
+    const actualizarState = () => {
+        console.log("Escribiendo...")
+    }
+
+    return (
+        <Fragment>
+            <h2>Crear Cita</h2>
+
+            <form>
+                <label>Nombre Mascota</label>
+                <input
+                    type="text"
+                    name="mascota"
+                    className="u-full-width"
+                    placeholder="Nombre mascota"
+                    onChange={actualizarState}
+                />
+                <label>Nombre Dueño</label>
+                <input
+                    type="text"
+                    name="propietario"
+                    className="u-full-width"
+                    placeholder="Nombre del dueño"
+                    onChange={actualizarState}
+                />
+                <label>Fecha</label>
+                <input
+                    type="date"
+                    name="fecha"
+                    className="u-full-width"
+                    onChange={actualizarState}
+                />
+                <label>Hora</label>
+                <input
+                    type="time"
+                    name="hora"
+                    className="u-full-width"
+                    onChange={actualizarState}
+                />
+                <label>Síntomas</label>
+                <textarea
+                    className="u-full-width"
+                    name="sintomas"
+                    onChange={actualizarState}
+                ></textarea>
+                <button
+                    type="submit"
+                    className="u-full-width button-primary"
+                >Agregar Cita</button>
+            </form>
+        </Fragment>
+    );
+}
+
+export default Formulario;
